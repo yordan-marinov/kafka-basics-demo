f3675d5cc6a7cf9e430bd16d7bbefbd187a874cd
yordan-marinov <jordanmarinov8@gmail.com>
2022-06-20 15:48:39 +0300

    Refactoring

--- kafka-producer-wikimedia/src/main/java/com/yordanm/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/com/yordanm/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -22,6 +22,11 @@ public class WikimediaChangesProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        // Set high throughput producer configs
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024));
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+
         // Create Producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
